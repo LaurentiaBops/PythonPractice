77c62fd17c00e3a16fdf239555e9931e3991f2bc
LaurentiaBops <bopela.nb@gmail.com>
2025-07-20 17:52:38 +0200

    Debug random number game to show number of attempts before winning

--- match_case.py
+++ match_case.py
@@ -21,11 +21,13 @@ match guess:
         print("Nope, your guess is a biy low. Give it another shot!")
 
 #Create a loop until the correct answer is guessed
+attempt = 1 #Initial counter for number of attempts
 
 while guess != secret_number:
     play_again =  input("Play again? (yes/no)")
     if play_again =="yes":
         guess =int(input("Guess a random number between 1 and 10: "))
+        attempt += 1
         match guess:
              case n if n == secret_number:
                  print("Congratulations you guessed it!")
@@ -35,7 +37,10 @@ while guess != secret_number:
                  print("Nope, your guess is a biy low. Give it another shot!")
       
     else:
-        break
+        break 
+
+if guess == secret_number:     #Ensures number of attempts is shown only if user finishes and wins game
+    print(f"Congratulations! You won the game in {attempt} attempts")    
     
 
       
